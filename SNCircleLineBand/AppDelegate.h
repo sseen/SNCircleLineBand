db6eb997e4d663f9b2397f5a4c7ecb8b02b5d7ff
MSDJ <sun.shuainan@mingshidianjin.com>
2015-08-11 16:31:47 +0800

    local to remote

---
+++ SNCircleLineBand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNCircleLineBand
+//
+//  Created by MSDJ on 15/8/11.
+//  Copyright (c) 2015年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNCircleLineBand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNCircleLineBand
+//
+//  Created by MSDJ on 15/8/11.
+//  Copyright (c) 2015年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
